b3ebdac7744473876427c7572d508d90f960a612
Eugeny.Horoshilov <eugeny.horoshilov@corp.mail.ru>
2014-11-20 19:44:37 +0300

    click directive

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -15,9 +15,43 @@
                 name : 'New Year Eve 2015',
                 date : '31.12.2014',
                 time : '20:00',
-                imageUrl : 'images/event-1.jpg'
+                imageUrl : 'images/event-1.jpg',
+                sessions: [
+                    {
+                        "name": "Soprano",
+                        "events": "fugiat Lorem consequat sunt consectetur",
+                        "duration": "30 min",
+                        "votes": 0
+                    },
+                    {
+                        "name": "Fangold",
+                        "events": "quis enim cupidatat et do",
+                        "duration": "60 min",
+                        "votes": 0
+                    },
+                    {
+                        "name": "Suremax",
+                        "events": "duis voluptate nisi culpa irure",
+                        "duration": "30 min",
+                        "votes": 0
+                    },
+                    {
+                        "name": "Ultrimax",
+                        "events": "consectetur sunt sit id nostrud",
+                        "duration": "30 min",
+                        "votes": 0
+                    }
+                ]
             };
 
+
+            $scope.voteUp = function(session){
+                session.votes++;
+            };
+
+            $scope.voteDown = function(session){
+                session.votes--;
+            };
         }
     );
 
