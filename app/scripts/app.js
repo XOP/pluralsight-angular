842c71372185d03b6731ce62c23cb8204f6e7505
Eugeny.Horoshilov <eugeny.horoshilov@corp.mail.ru>
2014-11-26 20:58:03 +0300

    filters and scss org

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,10 +8,25 @@
 
     var appName = 'Event Reg';
 
-    var app = angular.module('eventReg', ['event']);
+    var app = angular.module('eventReg', ['event', 'ngSanitize']);
 
     app.controller('appName', function(){
         this.name = appName;
+    });
+
+    app.controller('footerData', function($scope, $sce){
+
+        $scope.copyright = ''+
+            '<span title="' + appName + '">' +
+                '<span style="text-transform: uppercase; color: salmon;">' +
+                    'c' +
+                '</span>' +
+                'opyright' +
+            '</span>';
+
+        $scope.trustedAvailable = true;
+        $scope.copyrightTrusted = $sce.trustAsHtml($scope.copyright);
+
     });
 
 })();
--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -11,35 +11,44 @@
     app.controller('eventCtrl',
         function eventCtrl($scope){
 
+            $scope.votesStyle = {
+                'font-size' : '.8em'
+            };
+
             $scope.event = {
                 name : 'New Year Eve 2015',
                 date : '31.12.2014',
                 time : '20:00',
                 imageUrl : 'images/event-1.jpg',
+                price : '80',
                 sessions: [
                     {
                         "name": "Soprano",
                         "events": "fugiat Lorem consequat sunt consectetur",
                         "duration": "30 min",
-                        "votes": 0
+                        "votes": 0,
+                        special: false
                     },
                     {
                         "name": "Fangold",
                         "events": "quis enim cupidatat et do",
                         "duration": "60 min",
-                        "votes": 0
+                        "votes": 0,
+                        special: true
                     },
                     {
                         "name": "Suremax",
                         "events": "duis voluptate nisi culpa irure",
                         "duration": "30 min",
-                        "votes": 0
+                        "votes": 0,
+                        special: false
                     },
                     {
                         "name": "Ultrimax",
                         "events": "consectetur sunt sit id nostrud",
                         "duration": "30 min",
-                        "votes": 0
+                        "votes": 0,
+                        special: false
                     }
                 ]
             };
@@ -52,6 +61,7 @@
             $scope.voteDown = function(session){
                 session.votes--;
             };
+
         }
     );
 
